ca09e02fe4fd0949852a92024d92f8ac669013f6
Matias <matiasnegri85@gmail.com>
2021-06-09 22:20:39 -0300

    inico desde ComerciosdminSocialup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    vapidKey: 'BOHbXSuRE-89N26L35dHM_Ut0aiVjZhZTI2kfyt0Kw_gTrEC4O_EvW_a5cI8z2OKw62d-Ov7RrfhWgJPYv50WyY',
+    claveServidor: 'AAAA9Kup-lk:APA91bFYlNlzma5wp5aLISYAPHu_aroBrL8EuKmxdnVcyDQLx3gezF11rCfLpkgEpAXcB5YZMpbsGRmAeD6hx0Y-YTtI9edxKRMbNMnVS4uEv7vnyZlQr1vpb512lMT58cOZe2-lw6oT'
+  }
 };
